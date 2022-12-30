372a70e3edc733c80a1ab4fd24bcd095f84daeab
Aleja Ocampo <omaira.527@hotmail.com>
2022-12-29 20:10:28 -0500

    add category

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,17 +1,37 @@
 import { useState } from "react"
+import AddCategory from "./components/AddCategory"
 
 const GifExpertApp = () => {
 	const [categories, setCategories] = useState(["drag0n ball", "smile"])
-	const addNewCategory = () => {
+	//const [inputValue, setInputValue]=useState('')
+	/*const addNewCategory = () => {
 		setCategories([
 			...categories,
 			"hello"
 		])
+	}*/
+
+
+	const onAddNewCategory= (newCategory) => {
+		if(categories.includes(newCategory))return
+		setCategories([newCategory, ...categories])
+
 	}
+
+	/*const onSubmit = (e) => {
+		e.preventDefault()
+		if(inputValue.trim().length <= 1)return 
+		setCategories([
+			...categories,
+			inputValue
+		])
+		setInputValue('')
+	}*/
   return (
 	<>
 	<h2>GifExpertApp</h2>
-	<button onClick={addNewCategory}>Agregar</button>
+	<AddCategory  onNewCategory = {(value) => onAddNewCategory(value)}/>
+	<button onClick={addNewCategory} >Agregar</button>
 	{
 		categories.map(category => (
 			<ul>
@@ -19,6 +39,7 @@ const GifExpertApp = () => {
 			</ul>
 		))
 	}
+	
 	</>
   )
 }
